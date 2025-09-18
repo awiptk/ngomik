0c66a9377edc2c2dbecccb90cbdf295e78001f2e
awiptk <awiselow123@gmail.com>
2025-09-19 01:11:54 +0700

    Update ChapterActivity.kt

--- app/src/main/java/com/example/ngomik/ChapterActivity.kt
+++ app/src/main/java/com/example/ngomik/ChapterActivity.kt
@@ -15,6 +15,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.jsoup.Jsoup
+import com.example.ngomik.util.ImageUtils
 
 class ChapterActivity : AppCompatActivity() {
     private lateinit var recycler: RecyclerView
