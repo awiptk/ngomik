a67857b1b52324d05f906ef5e579cfe593dc546a
awiptk <awiselow123@gmail.com>
2025-10-01 21:02:58 +0700

    Create app/src/main/java/com/example/ngomik/WebViewActivity.kt

---
+++ app/src/main/java/com/example/ngomik/WebViewActivity.kt
@@ -0,0 +1,80 @@
+package com.example.ngomik
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+
+class WebViewActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+    private lateinit var progressBar: ProgressBar
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_webview)
+
+        val toolbar: Toolbar = findViewById(R.id.web_toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        progressBar = findViewById(R.id.web_progress)
+        webView = findViewById(R.id.webview)
+
+        // Konfigurasi WebView
+        val settings: WebSettings = webView.settings
+        settings.javaScriptEnabled = true
+        settings.domStorageEnabled = true
+        settings.useWideViewPort = true
+        settings.loadWithOverviewMode = true
+
+        // Ambil URL dari intent
+        val url = intent.getStringExtra("url") ?: "https://id.ngomik.cloud"
+
+        // Client supaya tetap di app
+        webView.webViewClient = WebViewClient()
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onProgressChanged(view: WebView?, newProgress: Int) {
+                if (newProgress < 100) {
+                    progressBar.visibility = View.VISIBLE
+                    progressBar.progress = newProgress
+                } else {
+                    progressBar.visibility = View.GONE
+                }
+            }
+
+            override fun onReceivedTitle(view: WebView?, title: String?) {
+                super.onReceivedTitle(view, title)
+                supportActionBar?.title = title
+            }
+        }
+
+        webView.loadUrl(url)
+    }
+
+    // Tombol back -> cek history WebView dulu
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    // Tombol back (panah di Toolbar)
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            onBackPressed()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
