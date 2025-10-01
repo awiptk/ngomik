e84839cd99977bc4c1c2232a6ba9c67adc5b146f
awiptk <awiselow123@gmail.com>
2025-10-01 15:25:55 +0700

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+// build.gradle.kts (root)
+
+buildscript {
+    val kotlin_version by extra("1.9.10")
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
